0dd13993b6fffc30b77767f80ead6df13c1c1632
seal323 <elizaveta.brus@binary-studio.com>
2021-12-14 22:49:45 +0200

    update lab3

--- lab3/task2.c
+++ lab3/task2.c
@@ -1,62 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h> 
-#include <unistd.h> 
-#include <time.h> 
-#include <sys/mman.h> 
-#include <sys/stat.h> 
-#include <fcntl.h> 
-
-struct datum_s {
-	pid_t pid;
-	struct tm cur_time;
-	char buff[256];
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/errno.h>
+int BUFFER_SIZE = 512;
+struct datum
+{
+  pid_t pid;
+  struct tm current_timestamp;
+  char buffer[256];
 };
 
-typedef struct datum_s datum_t;
-
-int main(void) {
+int main(void)
+{
   int fd;
-  char buff[256];
-  time_t rawtime;
-
+  char buffer[256];
+  struct datum *dtm = NULL;
+  time_t current_timestamp;
   fd = shm_open("/lab3", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-  
-  if (fd == -1) {
-      printf("Error while trying to shm_open!");
-      return -1;
+  if (fd == -1)
+  {
+    printf("Error: %s\n", strerror(errno));
+    return -1;
   }
-  
-  if (ftruncate(fd, sizeof(struct datum_s)) == -1) {
-      perror("ftruncate");
-      printf("Error while trying to ftruncate fd from shp_open!");
-      return -1;
+  if (ftruncate(fd, sizeof(struct datum)) != 0)
+  {
+    printf("Error: %s\n", strerror(errno));
+    return -1;
   }
-  
-  datum_t * datum = mmap(NULL, sizeof(struct datum_s), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-  if (datum == MAP_FAILED) {
-      printf("Error while trying to mmap() fd!");
-      return -1;
+  dtm = mmap(NULL, sizeof(struct datum), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (dtm == MAP_FAILED)
+  {
+    printf("Error: %s\n", strerror(errno));
+    return -1;
   }
-
-  while(1) {
-    printf("Please, enter some string:\n");
-    fgets(buff, 256, stdin);
-
-    msync(datum, sizeof(struct datum_s), MS_SYNC);
+  while (1)
+  {
+    printf("please, enter something:\n");
+    fgets(buffer, 512, stdin);
+    msync(dtm, sizeof(struct datum), MS_SYNC);
     printf("Data in datum:\n");
-    printf("PID: %d\n", datum->pid);
-    printf("Time %s", asctime(&(datum->cur_time)));
-    printf("String %s\n", datum->buff);
-  
-    datum->pid = getpid();
-    time(&rawtime);
-    datum->cur_time = (* localtime(&rawtime));
-    strcpy(datum->buff, buff);
+    printf("PID: %d\n", dtm->pid);
+    printf("Current time: %s\n", asctime(&(dtm->current_timestamp)));
+    printf("String from buffer: %s\n", dtm->buffer);
+    dtm->pid = getpid();
+    time(&current_timestamp);
+    dtm->current_timestamp = (*localtime(&current_timestamp));
+
+    strcpy(dtm->buffer, buffer);
   }
-  
-  munmap(datum, sizeof(struct datum_s));
+  munmap(dtm, sizeof(struct datum));
   shm_unlink("/lab3");
   return 0;
 }
